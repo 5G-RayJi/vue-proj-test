0e760f716ead1544d8d51fe7da86b305388e28c2
DX-0041\RayJi <rayji@5gg.win>
2024-09-24 13:57:52 +0800

    auto-imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,34 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//unplugin-auto-import
+import AutoImport from 'unplugin-auto-import/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      // 在哪些檔案裡可以直接引入
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      // 需要自動匯入的插件，自訂匯入的API
+      imports: [
+        'vue',
+        'vue-i18n',
+        'vue-router',
+        'pinia',
+        {
+          '@/dayjsSetup': [['default', 'dayjs']],
+          lodash: [['default', '_']]
+        }
+      ],
+      dts: './src/types/auto-imports.d.ts'
+    })
   ],
   resolve: {
     alias: {
